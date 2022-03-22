f8e1043f23835d87197001591f62232bff41c2f1
miguegonzalez <miguel.goncon@gmail.com>
2022-03-22 20:32:14 +0100

    create Tab_3

---
+++ src/components/Tab_3.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="row">
+    <div class="col">
+      <lxBeerCard 
+          :image_url="show.image_url" 
+          :name_beer="show.name" 
+          :description="show.description" 
+          :food_pairing="show.food_pairing" 
+          :abv="show.abv" 
+          v-for="(show, index) in myJson" 
+          :key="show.id" 
+          :active="index === activeLiIndex"
+          :index="index"
+          @newactive="activeLiIndex = $event"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import lxBeerCard from './lx-beer-card.vue'
+import myJson from './Tab_3.json'
+
+
+export default {
+  name: 'Tab_1',
+  props: {},
+  components: {
+      lxBeerCard,
+  },
+  data() {
+    return {
+        activeLiIndex: null,
+        myJson: myJson,
+    }
+  },
+}
+</script>
+
---
+++ src/components/lx-beer-card.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="lx-beer-card">
+    <div class="cols-auto lx-beer-card__image"><img :src="image_url" /></div>
+    <div class="col flex-separate-column">
+      <div class="row">
+        <div class="col">
+          <div class="lx-beer-card__name-beer">{{ name_beer }}</div>
+          <div class="lx-beer-card__description">{{ description }}</div>
+        </div>
+      </div>
+      <div class="row lx-beer-card__food-pairing">
+          <div>Ideal para combinar con:&nbsp;</div>
+          <div v-for="(show, index) in food_pairing" :key="index">
+            <span v-if="index == Object.keys(food_pairing).length - 1" >&nbsp;y </span>
+            <span v-else-if="index != 0">, </span>
+            <span> {{ show }}</span>
+            <span v-if="index == Object.keys(food_pairing).length - 1">.</span>
+          </div>
+      </div>
+    </div>
+    <div class="cols-auto lx-beer-card__abv" :class="bgAbv()"> {{ abv }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'lx-beer-card',
+  props: {
+    image_url: String,
+    name_beer: String,
+    description: String,
+    food_pairing: Array,
+    abv: Number,
+  },
+  methods: {
+    bgAbv() {
+      var abv_value;
+
+      if(this.abv <= "5.0") {
+        abv_value = 'bg-warning';
+      }
+      else if(this.abv >= "10.0") {
+        abv_value = 'bg-danger';
+      }
+      else {
+        abv_value = 'bg-info';
+      }
+      return abv_value;
+    }
+  }
+}
+</script>
