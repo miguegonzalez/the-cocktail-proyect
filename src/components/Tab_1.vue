ac222861b39a50745f7d0af5fd3df0cbc846fe77
miguegonzalez <miguel.goncon@gmail.com>
2022-03-22 19:03:46 +0100

    dev of the view tab1

---
+++ src/components/Tab_1.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="row">
+    <lxRadioButton 
+        :title="show.title" 
+        :description="show.description" 
+        v-for="(show, index) in myJson" 
+        :key="show.id" 
+        :active="index === activeLiIndex"
+        :index="index"
+        @newactive="activeLiIndex = $event"
+    />
+  </div>
+</template>
+
+<script>
+import lxRadioButton from './lx-radio-button.vue'
+import myJson from './Tab_1.json'
+
+
+export default {
+  name: 'Tab_1',
+  props: {},
+  components: {
+      lxRadioButton,
+  },
+  data() {
+    return {
+        activeLiIndex: null,
+        myJson: myJson,
+    }
+  },
+}
+</script>
+
---
+++ src/components/lx-radio-button.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="lx-radio-button col h-100" :class="[{'active': active}, index]" @click.stop="toogleRowActive">
+    <div class="row lx-radio-button__title">{{ title }}</div>
+    <div class="row lx-radio-button__description">
+      <ul>
+        <li v-for="(show, index) in description" :key="index">{{ show }}</li>
+      </ul>
+    </div>
+    <div class="row lx-radio-button__button">
+      <input type="radio" name="choose_scheme" value="Elegir régimen" :checked="active" /> 
+      <label for="choose_scheme">Elegir régimen</label>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'lx-radio-button',
+  props: {
+    title: String,
+    description: Array,
+    active: Boolean,
+    index: Number,
+  },
+  methods: {
+    toogleRowActive() {
+      this.$emit('newactive', this.index)
+    }
+  }
+}
+</script>
